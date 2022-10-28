32aed925d1ba901beef623b606e409a6166db936
Martin Lo <46683490+MartinLLo@users.noreply.github.com>
2022-10-28 11:53:55 -0500

    Add files via upload

---
+++ S1-Organise&Sort/S1.3-delete_series.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+S1.3 delete_series.py
+
+in this script you'll be able to call the deleteseries function and enter the 
+desired series number in the function parameters to delete the scans with the 
+corresponding series number in the metadata
+
+@author: m3lo4
+"""
+import os
+import time
+import pydicom
+import pandas as pd
+
+def delete_series(directory, series_number):
+    st = time.time()
+    
+    file_list = list()
+    file_notdcm = list()
+
+    for root, dirs, files in os.walk(directory):
+        for name in files:
+            if name.lower().endswith('.dcm'):
+                file_list.append(os.path.join(root, name))
+                # print(file_list)
+            else:
+                file_notdcm.append(os.path.join(root,name))
+     
+    lst = []   
+    
+    for file in file_list[:]:
+        if pydicom.dcmread(file).SeriesNumber == 1:
+            print('yes')
+            x = os.path.abspath(file) + ' is a topogram and will be removed'
+            lst.append(x)
+            # os.remove(file)
+                    
+    print(len(lst))
+    df = pd.DataFrame(lst)
+    df.to_csv('Series_deleted.csv')
+
+    et = time.time()
+    elapsed_time = et - st
+    print('Script took:', elapsed_time, 'seconds')
+    
+
+directory = r'C:\Users\m3lo4\OneDrive\UCL\PhD\Practice_Data\Bugfixing'   
+ 
+delete_series(directory, 1)
